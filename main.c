35c4e5314de2a8c5a1d09b93ff3148cc171985ba
Billy Hazelwood <billy.j.hazelwood@gmail.com>
2025-12-26 00:12:38 -0500

    created DS for packet

--- main.c
+++ main.c
@@ -10,7 +10,6 @@
 
 
 // DHCP header structure
-
 typedef struct DHCP
 {
     uint8_t opcode;
@@ -72,6 +71,7 @@ typedef struct Packet
 } __attribute__((packed)) Packet;
 
 void spoof_mac(uint8_t *mac);
+void rand_transaction_id(uint8_t *id);
 
 int main(void)
 {
@@ -81,6 +81,8 @@ int main(void)
         perror("malloc");
         return 1;
     }
+    
+    srand(time(NULL));
 
     // init Ethernet header
     spoof_mac(p->eth.src_mac);
@@ -105,14 +107,42 @@ int main(void)
     memset(p->ip.dst_ip, 0xff, sizeof(p->ip.dst_ip));
 
     // init udp header
-    
+    p->udp.src_port = htons(68);
+    p->udp.dst_port = htons(67);
+    p->udp.len = htons(8 + 552);
+    p->udp.checksum = 0;
+
+    // init dhcp header
+    p->dhcp.opcode = 1;
+    p->dhcp.hw_type = 1;
+    p->dhcp.hw_len = 6;
+    p->dhcp.hops = 0;
+    rand_transaction_id(p->dhcp.transaction_id);
+    memset(p->dhcp.sec_elapsed, 0x00, sizeof(p->dhcp.sec_elapsed));
+    memset(p->dhcp.flags, 0x00, sizeof(p->dhcp.flags));
+    memset(p->dhcp.client_ip, 0x00, sizeof(p->dhcp.client_ip));
+    memset(p->dhcp.server_ip, 0x00, sizeof(p->dhcp.server_ip));
+    memset(p->dhcp.gtwy_ip, 0x00, sizeof(p->dhcp.gtwy_ip));
+    memcpy(p->dhcp.client_mac, p->eth.src_mac, sizeof(p->eth.src_mac));
+    memset(p->dhcp.server_name, 0x00, sizeof(p->dhcp.server_name));
+    memset(p->dhcp.boot_file, 0x00, sizeof(p->dhcp.boot_file));
+    memset(p->dhcp.ip_addr, 0x00, sizeof(p->dhcp.ip_addr));
+    p->dhcp.magic_cookie[0] = 99;
+    p->dhcp.magic_cookie[1] = 130;
+    p->dhcp.magic_cookie[2] = 83;
+    p->dhcp.magic_cookie[3] = 99;
+    p->dhcp.options[0] = 53;
+    p->dhcp.options[1] = 1;
+    p->dhcp.options[2] = 1;
+    p->dhcp.options[3] = 255;
+    memset(&p->dhcp.options[4], 0x00, 308);
+
+
 
 }
 
 void spoof_mac(uint8_t *mac)
 {
-    srand(time(NULL));
-
     for(int i = 0; i < 6; i++)
     {
         mac[i] = rand() % 256;
@@ -120,4 +150,12 @@ void spoof_mac(uint8_t *mac)
     // clear the unicast bit and set the locally administered bit
     mac[0] = (mac[0] & 0xfe) | 0x02;
 
+}
+
+void rand_transaction_id(uint8_t *id)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        id[i] = rand() % 256;
+    }
 }
